f699dc6a996f39655fdceb1e9cd2698554db0dd0
Baiochi <jao.baiochi@gmail.com>
2022-07-06 14:54:55 -0300

    add plot and description pages

--- utils/st_render.py
+++ utils/st_render.py
@@ -1,14 +1,80 @@
-
-
+import json
+# Math operations
+import numpy as np
+# Regex operations
+import re
+# Reading and process data
+import pandas as pd
+# Data visualization
+import plotly.express as px
+from utils.urban_layout import CATEGORICAL_GROUPS, LAYOUT_SPECS, SHADES
+# Web rendering API
 import streamlit as st
 from utils.css_style import h1_style, h2_style, hr_style, link_style
+# Preprocess function
+from utils.data_preprocess import clean_data
 
-import plotly.express as px
+# Load data
+df = pd.read_json('data/consumo.json')
+with open('data/consumo.json', 'r') as file:
+	json_data = json.load(file)
+# Clean data
+data_frame = clean_data(df, json_data)
 
-
+# Normalize dataframe after selecting client
+def normalize_query(query_data):
+	# Normalizing tarifas
+	query_data = query_data.explode('tarifas')
+	query_data = pd.concat([
+	query_data.drop(columns='tarifas'),
+	query_data['tarifas'].apply(pd.Series)
+	], axis=1)
+	# Normalizing estruturaConsumo
+	query_data = pd.concat([
+	query_data.drop(columns='estruturaConsumo'),
+	query_data['estruturaConsumo'].apply(pd.Series)
+	], axis=1)
+	return query_data
 
 def show_dashboard_page():
-	pass
+
+	st.markdown(h1_style('Distribuição das medidas cliente'), unsafe_allow_html=True)
+
+	cliente = st.selectbox(
+		'Escolha o nome do cliente:',
+		sorted(data_frame['cliente'].unique()))
+
+	# Query by client
+	query_data = data_frame[data_frame['cliente']==cliente]
+	# Normalize dataframe
+	query_data = normalize_query(query_data)
+
+	# Creating saldo_medido feature
+	query_data['saldo_medido'] = query_data['faturado'] - query_data['medido']
+
+	# Creating plotly figure
+	fig = px.box(
+		data_frame=query_data, 
+		x='posto', 
+		y='saldo_medido', 
+		color='un',
+		title='Distribuição do saldo medido por grupo', 
+		width=800, 
+		#color_discrete_sequence=CATEGORICAL_GROUPS[query_data['un'].nunique()]
+	).update_layout(**LAYOUT_SPECS)
+	# Render plot
+	st.plotly_chart(fig, use_container_width=True)
+
+	#Dataframe preview
+	st.markdown(h2_style('Visualizando o Dataframe'), unsafe_allow_html=True)
+	with st.expander('Filtre por coluna'):
+		diplay_columns = st.multiselect(
+			label='Selecione as colunas:',
+			options=query_data.columns,
+			default=list(query_data.columns)
+		)
+	st.dataframe(query_data[diplay_columns])
+
 
 def show_description_page():
 
@@ -43,7 +109,8 @@ A tarifa nesse período é mais cara.
 **TE**: Tarifa de Energia (KWh)  
 **TUSD**: Tarifa de Uso do Sistema de Distribuição  
 
-**Energia consumida**: parcelas $TE$ e $TUSD_{energia}$ em $R$/KWh$  
+**Energia consumida**: parcelas $TE$ e $TUSD_{energia}$ em $R$/KWh$   
+
 **Demanda contratada**: apenas a parcela $TUSD_{demanda}$ em $R$/KW$  
   
 <font color='#1696D2'>*Tarifa Azul*
@@ -93,12 +160,8 @@ Outras são inependentes dessa classificação:
 ### Questões a serem analisadas:
 - Consumo (KWh) é maior na PONTA ou FPONTA?  
 - Demanda contratada (KW) está sendo utilizada acima ou abaixo?  
-- Relação entre a diferença do valor faturado e o valor medido. Se o valor medido for maior, 
-qual o motivo do faturado ser menor? E no caso contrário? Existe algum comportamento comum para cada caso?
-- Como se comporta a fatura onde há alto valor medido de energia injetada(INJ)? 
-Essa energia é proveniente de geração própria?  
+- Relação entre a diferença do valor faturado e o valor medido. Se o valor medido for maior, qual o motivo do faturado ser menor? E no caso contrário? Existe algum comportamento comum para cada caso?
+- Como se comporta a fatura onde há alto valor medido de energia injetada(INJ)? Essa energia é proveniente de geração própria?  
 	''', unsafe_allow_html=True)
-
-
 
 
